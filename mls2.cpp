f1a6e32fb229f3671c0ee6819ab2080b4ebdce9a
SujeathPareddy <p.sujeath@gmail.com>
2015-01-10 23:20:10 +0530

    ->Significantly more vectorized code. ->Fixed a bug that prevented deformations from fully aligning with control points. ->Removed helper typedefs.

--- mls2.cpp
+++ mls2.cpp
@@ -15,19 +15,6 @@
 #include<boost/thread/thread.hpp>
 #include<boost/tuple/tuple.hpp>
 
-typedef Eigen::MatrixXf Matrix;
-typedef Eigen::VectorXf Vector;
-
-typedef pcl::PointXYZ PointPCL;
-typedef pcl::PointCloud<PointPCL> PointCloud;
-typedef PointCloud::Ptr Ptr;
-typedef pcl::visualization::PCLVisualizer Visualizer;
-
-using std::cout;
-using std::endl;
-using boost::tuple;
-using boost::tuples::get;
-
 template<class PointT>
 class MLSDeform{
 private:
@@ -36,24 +23,24 @@ private:
 	const typename pcl::PointCloud<PointT>::Ptr shifted;
 	const float alpha;
 
-	const float eucdist(const PointPCL &a,const PointPCL &b){
+	const float eucdist(const pcl::PointXYZ &a,const pcl::PointXYZ &b){
 		return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)+(a.z-b.z)*(a.z-b.z));
 	}
 
-	inline PointPCL E2P(const Matrix &in){
-		return PointPCL(in(0,0),in(1,0),in(2,0));
+	inline PointT E2P(const Eigen::MatrixXf &in){
+		return PointT(in(0,0),in(1,0),in(2,0));
 	}
 
-	inline Eigen::Matrix<float,3,1> P2E(const PointPCL &in){
+	inline Eigen::Matrix<float,3,1> P2E(const pcl::PointXYZ &in){
 		Eigen::Matrix<float,3,1> out;
 		out<<in.x,in.y,in.z;
 		return out;
 	}
 
-	Matrix PointCloud2Eigen(Ptr cloud){
-		Matrix out(3,cloud->size());
+	Eigen::MatrixXf PointCloud2Eigen(const typename pcl::PointCloud<PointT>::Ptr cloud){
+		Eigen::MatrixXf out(3,cloud->size());
 		int j=0;
-		for(PointCloud::iterator i=cloud->begin();i<cloud->end();i++){
+		for(typename pcl::PointCloud<PointT>::iterator i=cloud->begin();i<cloud->end();i++){
 			Eigen::MatrixXf temp(3,1);
 			temp<<i->x,i->y,i->z;
 			out.col(j++) = temp;
@@ -93,59 +80,61 @@ public:
 		Eigen::MatrixXf P = PointCloud2Eigen(original);
 		Eigen::MatrixXf Q = PointCloud2Eigen(shifted);
 		Eigen::Matrix3f PQt;
+		Eigen::Vector3f Pstar, Qstar;
+		const Eigen::MatrixXf weightSum = weights.rowwise().sum();
+		Eigen::ArrayXf temp = P;
 
 		for(int n=0;n<cloudSize;n++){
-			PQt.setZero();
-			Eigen::Vector3f Pstar, Qstar;
-			for(int i=0;i<controlSize;i++){
-				PQt += weights(n,i)*P.col(i)*(Q.col(i).transpose());
-				Pstar+=weights(n,i)*P.col(i);
-				Qstar+=weights(n,i)*Q.col(i);
-			}
-			const float weightSum = weights.row(n).sum();
-			Pstar/=weightSum;
-			Qstar/=weightSum;
+			//PQt.setZero();
+			Pstar = (weights.row(n)*P.transpose()).transpose()/weightSum(n);
+			Qstar = (weights.row(n)*Q.transpose()).transpose()/weightSum(n);
+			PQt = ((P.colwise()-Pstar).array().rowwise()*weights.row(n).array())
+				     .matrix()*(Q.colwise()-Qstar).transpose();
 
-			Eigen::JacobiSVD<Matrix> svd(PQt, Eigen::ComputeFullU | Eigen::ComputeFullV);
+			Eigen::JacobiSVD<Eigen::MatrixXf> svd(PQt, Eigen::ComputeFullU | Eigen::ComputeFullV);
 			float scaling = svd.singularValues().trace()/(P.transpose()*P).trace();
-			final->push_back(E2P((svd.matrixV()*svd.matrixU().transpose()*(P2E(cloud->points[n])-Pstar)+Qstar)));
+			final->push_back(E2P((svd.matrixV()*svd.matrixU().transpose()*
+			                  ((cloud->points[n]).getVector3fMap()-Pstar)*scaling*2+Qstar)));
 			}
 		return final;
 		}
 };
 
-boost::shared_ptr<Visualizer> simpleVis (PointCloud::ConstPtr cloud){
-	boost::shared_ptr<Visualizer> viewer (new Visualizer ("3D Viewer"));
+boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis
+  (typename pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud){
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer
+	  (new pcl::visualization::PCLVisualizer ("3D Viewer"));
 	viewer->setBackgroundColor (0, 0, 0);
-	viewer->addPointCloud<PointPCL> (cloud, "sample cloud");
+	viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
 	viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
 	//viewer->addCoordinateSystem (0.1);
 	viewer->initCameraParameters ();
 	return (viewer);
 }
 
-tuple<Ptr,Ptr,Ptr> createPlateCloud(float Ra,int rc=100,int ac=100,float X = 0.0,float Y = 0.0,float Z = 0.0){
-	Ptr out(new PointCloud);
-	Ptr center(new PointCloud);
-	Ptr rim(new PointCloud);
-	Vector T(ac,1);
+boost::tuple< pcl::PointCloud<pcl::PointXYZ>::Ptr,pcl::PointCloud<pcl::PointXYZ>::Ptr,
+	pcl::PointCloud<pcl::PointXYZ>::Ptr >
+	createPlateCloud(float Ra,int rc=100,int ac=100,float X = 0.0,float Y = 0.0,float Z = 0.0){
+	pcl::PointCloud<pcl::PointXYZ>::Ptr out(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr center(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr rim(new pcl::PointCloud<pcl::PointXYZ>());
+	Eigen::VectorXf T(ac,1);
 	T.setLinSpaced(100,0,6.2830);
-	Vector R(rc,1);
+	Eigen::VectorXf R(rc,1);
 	R.setLinSpaced(rc,0,Ra);
 	for(int t=0;t<T.rows();t++){
 		for(int r=0;r<R.rows();r++){
 			if((r>=0 && r<=3)){
-				center->push_back(PointPCL(X+R(r,0)*sin(T(t,0)),Y+R(r,0)*cos(T(t,0)),Z));
+				center->push_back(pcl::PointXYZ(X+R(r,0)*sin(T(t,0)),Y+R(r,0)*cos(T(t,0)),Z));
 			}
 			else if(r>R.rows()-7){
-				rim->push_back(PointPCL(X+R(r,0)*sin(T(t,0)),Y+R(r,0)*cos(T(t,0)),Z));
+				rim->push_back(pcl::PointXYZ(X+R(r,0)*sin(T(t,0)),Y+R(r,0)*cos(T(t,0)),Z));
 			}
-			else{
-				out->push_back(PointPCL(X+R(r,0)*sin(T(t,0)),Y+R(r,0)*cos(T(t,0)),Z));
-			}
+		 out->push_back(pcl::PointXYZ(X+R(r,0)*sin(T(t,0)),Y+R(r,0)*cos(T(t,0)),Z));
 		}
 	}
-	return tuple<Ptr,Ptr,Ptr>(out,center,rim);
+	return boost::tuple< pcl::PointCloud<pcl::PointXYZ>::Ptr,pcl::PointCloud<pcl::PointXYZ>::Ptr,
+			pcl::PointCloud<pcl::PointXYZ>::Ptr >(out,center,rim);
 }
 
 int main(int argv,char *args[]){
@@ -155,32 +144,34 @@ int main(int argv,char *args[]){
 	}
 	const float alpha = atof(args[1]);
 
-	tuple<Ptr,Ptr,Ptr> temp = createPlateCloud(5);
-	Ptr cloud = get<0>(temp);
-	Ptr center = get<1>(temp);
-	Ptr rim = get<2>(temp);
-	Ptr original(new PointCloud);
-	Ptr shifted(new PointCloud);
+	boost::tuple<pcl::PointCloud<pcl::PointXYZ>::Ptr,pcl::PointCloud<pcl::PointXYZ>::Ptr,
+							  pcl::PointCloud<pcl::PointXYZ>::Ptr> temp =
+								createPlateCloud(5,1000,1000);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = boost::tuples::get<0>(temp);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr center = boost::tuples::get<1>(temp);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr rim = boost::tuples::get<2>(temp);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr original(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr shifted(new pcl::PointCloud<pcl::PointXYZ>);
 	const long cloudSize = cloud->size();
 
 	original->insert(original->end(),rim->begin(),rim->end());
 	original->insert(original->end(),center->begin(),center->end());
 
-	for(PointCloud::iterator i = center->begin();i < center->end();i++){
+	for(pcl::PointCloud<pcl::PointXYZ>::iterator i = center->begin();i < center->end();i++){
 		i->z-=2;
 	}
 
 	shifted->insert(shifted->end(),rim->begin(),rim->end());
 	shifted->insert(shifted->end(),center->begin(),center->end());
 
+  std::cout<<"Beginning deformation"<<std::endl;
 	MLSDeform<pcl::PointXYZ> deformer(cloud,original,shifted,alpha);
-	Ptr final(deformer.deform());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr final(deformer.deform());
+	std::cout<<"Done!"<<std::endl;
 
-	std::cout<<final->size()<<std::endl;
-
-	boost::shared_ptr<Visualizer> v = simpleVis(final);
-	v->addPointCloud<PointPCL>(original,"original");
-	v->addPointCloud<PointPCL>(shifted,"modified");
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> v = simpleVis(final);
+	v->addPointCloud<pcl::PointXYZ>(original,"original");
+	v->addPointCloud<pcl::PointXYZ>(shifted,"modified");
 	while(!v->wasStopped()){
 		v->spinOnce(100);
 		boost::this_thread::sleep(boost::posix_time::microseconds(10000));
